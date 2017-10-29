f074701aa5341e906c9ab88f4fbce22a5698d15c
Mikael Kulma <mikael.kulma@ambientia.fi>
2017-10-29 14:12:43 +0200

    The string #numbers returns #sum

--- src/test/groovy/com/mikaelkulma/kata/stringcalculator/StringCalculatorSpec.groovy
+++ src/test/groovy/com/mikaelkulma/kata/stringcalculator/StringCalculatorSpec.groovy
@@ -1,6 +1,7 @@
 package com.mikaelkulma.kata.stringcalculator;
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class StringCalculatorSpec extends Specification {
 
@@ -19,5 +20,18 @@ class StringCalculatorSpec extends Specification {
     def "Two numbers return their sum"() {
         expect:
         calculator.add("1,2") == 3
+    }
+
+    @Unroll
+    def "The string #numbers returns #sum"() {
+        expect:
+        calculator.add(numbers) == sum
+
+        where:
+        numbers             | sum
+        "1,2,3,4,5,6,7,8"   | 36
+        "10,34,200"         | 244
+        "87,23,3,1,3,3,3"   | 123
+        "1,1,1,1,1,1,1,1,1" | 9
     }
 }
